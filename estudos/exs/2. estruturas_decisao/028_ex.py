29e472b9e0d76d5557315e3f470a02c69c5ffaed
wpceid <wpceid@gmail.com>
2025-09-16 18:50:28 -0300

    Usando o randint para deixar o código dinâmico

--- estudos/exs/2. estruturas_decisao/028_ex.py
+++ estudos/exs/2. estruturas_decisao/028_ex.py
@@ -1,9 +1,11 @@
 #Escolha um número entre 0 e 5 e peça para o usuário adivinhar e informe se ele acertou ou errou
 
-num_certo = 2
+from random import randint
+
+num_certo = randint(0, 5)
 
 num_escolhido = int(input("Escolha um número entre 0 e 5: "))
 
 if num_certo == num_escolhido:
- print("Você acertou!")
-else: print("Você errou!")
+ print(f"Você acertou! Eu pensei no número {num_certo}")
+else: print(f"Você errou! Eu pensei no número {num_certo}")
